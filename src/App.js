619440e9d3a8403e2f755f205f19a5ec30afd6ce
svishere <sakthivel100gym@gmail.com>
2023-04-18 11:50:37 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react'
+import Axios from "axios"
+import "./App.css"
 
-function App() {
+
+const APIkey ="5bc5a54797e004c1241b4bcd189dd7e3";
+
+const App = () => {
+
+  const[city,setCity] = useState("");
+  const[data,setData] = useState();
+
+  const fetchData = async () => {
+    try{
+      const response = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`);
+      setData(response.data);
+      console.log(response);
+    }
+    catch(err){
+      alert("Please Enter The City Name Correct");
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='css'>
+    <div className='App'>
+    <h1 className='title'>Weather App</h1>
+    <div className='input-container'>
+      <input 
+      type="text"
+      className='input'
+      value={city}
+      onChange={e => setCity(e.target.value)}
+      placeholder="Enter The City Name"
+      />
+
+      <button className="button" onClick={fetchData}>Fetch</button>
+      
     </div>
-  );
+
+    <div>
+     {data && (
+        <div className='container'>
+          <h1 className='city-name'>{data.name},{data.sys.country}</h1>
+          <div className='temp'>{Math.round(data.main.temp)} C</div>
+          <div className='coordinated'>
+            <div>Lat - {data.coord.lat}</div>
+            <div>Lon - {data.coord.lon}</div>
+          </div>
+        </div>
+      )}
+
+    </div> 
+
+
+    </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
